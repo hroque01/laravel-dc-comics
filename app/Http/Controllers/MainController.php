0484fb0f533d3d6b431d89d7660a109a2d43155b
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-06 19:04:23 +0100

    Print names on frontend

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Person;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+        $people = Person::all();
+
+        return view('pages.home', compact('people'));
+    }
+}
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.main-layouts')
 
 @section('main')
-    <h1>PROVA MAIN</h1>
+    <h1>People</h1>
+    <ul>
+        @foreach ($people as $person)
+            <li>
+                {{ $person -> firstName }} {{ $person -> lastName}}
+            </li>
+        @endforeach
+    </ul>
 @endsection
